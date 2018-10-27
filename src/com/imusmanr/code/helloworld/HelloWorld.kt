9632f48398576e4fb065bd7ec6404268fad99fec
Rafique <usmanr@amazon.com>
2018-10-27 16:04:03 -0700

    Update printed output to indicate more coming!

--- src/com/imusmanr/code/helloworld/HelloWorld.kt
+++ src/com/imusmanr/code/helloworld/HelloWorld.kt
@@ -2,5 +2,5 @@ package com.imusmanr.code.helloworld
 
 fun main(args: Array<String>) {
     println("Hello, world!")
-    println("This is my Kotlin program.")
+    println("This is my first Kotlin application. Lot's more to come!")
 }
